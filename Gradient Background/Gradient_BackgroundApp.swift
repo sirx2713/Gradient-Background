bacd49e8e6d64a34ebc8199e0e0ace9317a57e45
Mabende <belindamabende@gmail.com>
2024-06-12 20:21:04 +0200

    Initial Commit

---
+++ Gradient Background/Gradient_BackgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gradient_BackgroundApp.swift
+//  Gradient Background
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/06/12.
+//
+
+import SwiftUI
+
+@main
+struct Gradient_BackgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Gradient BackgroundTests/Gradient_BackgroundTests.swift
@@ -0,0 +1,17 @@
+//
+//  Gradient_BackgroundTests.swift
+//  Gradient BackgroundTests
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/06/12.
+//
+
+import Testing
+@testable import Gradient_Background
+
+struct Gradient_BackgroundTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
